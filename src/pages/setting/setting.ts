17666adbf40452d6af7babb699db2dc7fcaea18d
terencehyz <1455723823@qq.com>
2017-08-14 11:06:15 +0800

    update

--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { RegisterPage } from "../register/register";
 
 /**
  * Generated class for the SettingPage page.
@@ -16,6 +17,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SettingPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  goRegister(){
+    this.navCtrl.push(RegisterPage);
   }
 
   ionViewDidLoad() {
