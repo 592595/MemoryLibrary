dad37365e1f12f461293e1d31d79e49c1bf8c31f
terencehyz <1455723823@qq.com>
2017-09-20 21:43:24 +0800

    add museum service

---
+++ src/providers/museum/museum.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the MuseumProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular DI.
+*/
+@Injectable()
+export class MuseumProvider {
+
+  public mList:any;
+  public isEmpty:boolean=false;
+
+  constructor(public http: Http) {
+    console.log('Hello MuseumProvider Provider');
+  }
+
+  getNear(){
+    let url = '/api/museum/nearBy';
+    this.http.get(url).subscribe((res)=>{
+      if(res.json().status){
+        if(res.json().data.length){
+          this.mList = res.json().data;
+        }
+        else{
+          this.isEmpty = true;
+        }
+      }
+      else{
+        this.isEmpty = true;
+      }
+    })
+  }
+
+}
